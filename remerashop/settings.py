c9f089cb6ed2b45ac1f40330274f4e6eb0a30cfb
fernandojbcba <benavidez.fernando@hotmail.com>
2023-07-04 21:05:44 -0300

    sqllite

--- remerashop/settings.py
+++ remerashop/settings.py
@@ -77,18 +77,22 @@ WSGI_APPLICATION = 'remerashop.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-
 DATABASES = {
-'default': {
-            'ENGINE': 'django.db.backends.mysql',
-            'NAME': 'fernandobenavide$remerasshop',
-            'USER': 'fernandobenavide',
-            'PASSWORD': 'remerashop1234',
-            'HOST': 'fernandobenavidez.mysql.pythonanywhere-services.com',
-            'PORT': '3306',
-            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
-        }
-}
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }}
+#DATABASES = {
+#'default': {
+#            'ENGINE': 'django.db.backends.mysql',
+#            'NAME': 'fernandobenavide$remerasshop',
+#            'USER': 'fernandobenavide',
+#            'PASSWORD': 'remerashop1234',
+#            'HOST': 'fernandobenavidez.mysql.pythonanywhere-services.com',
+#            'PORT': '3306',
+#            'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
+#        }
+#}
 
 
 # Password validation
