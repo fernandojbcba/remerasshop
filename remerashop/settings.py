768f39afdacce753e8ff774777faf90a4f1190cf
fernandojbcba <benavidez.fernando@hotmail.com>
2023-07-05 15:06:20 -0300

    sql

--- remerashop/settings.py
+++ remerashop/settings.py
@@ -77,12 +77,21 @@ WSGI_APPLICATION = 'remerashop.wsgi.application'
 
 
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+#DATABASES = {
+ #   'default': {
+  #      'ENGINE': 'django.db.backends.sqlite3',
+   #     'NAME': BASE_DIR / 'db.sqlite3',
+    #}}
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }}
-
+'default': {
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'remerashop$remerashop',
+            'USER': 'remerashop',
+            'PASSWORD': 'Argentina2013',
+            'HOST': 'remerashop.mysql.pythonanywhere-services.com',
+            'PORT': '3306',
+        }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
